0f550001dcbd091504f80d1ba5bc5b52baa96b3d
Marco Mihai Condrache <52580954+marcocondrache@users.noreply.github.com>
2025-12-14 20:39:31 +0100

    add setting
    Signed-off-by: Marco Mihai Condrache <52580954+marcocondrache@users.noreply.github.com>

--- crates/editor/src/editor_settings.rs
+++ crates/editor/src/editor_settings.rs
@@ -40,6 +40,7 @@ pub struct EditorSettings {
     pub scroll_sensitivity: f32,
     pub mouse_wheel_zoom: bool,
     pub fast_scroll_sensitivity: f32,
+    pub smooth_scroll: bool,
     pub sticky_scroll: StickyScroll,
     pub relative_line_numbers: RelativeLineNumbers,
     pub seed_search_query_from_cursor: SeedQuerySetting,
@@ -266,6 +267,7 @@ impl Settings for EditorSettings {
             scroll_sensitivity: editor.scroll_sensitivity.unwrap(),
             mouse_wheel_zoom: editor.mouse_wheel_zoom.unwrap(),
             fast_scroll_sensitivity: editor.fast_scroll_sensitivity.unwrap(),
+            smooth_scroll: editor.smooth_scroll.unwrap(),
             sticky_scroll: StickyScroll {
                 enabled: sticky_scroll.enabled.unwrap(),
             },
--- crates/settings/src/vscode_import.rs
+++ crates/settings/src/vscode_import.rs
@@ -297,6 +297,7 @@ impl VsCodeSettings {
             scroll_beyond_last_line: None,
             mouse_wheel_zoom: self.read_bool("editor.mouseWheelZoom"),
             scroll_sensitivity: self.read_f32("editor.mouseWheelScrollSensitivity"),
+            smooth_scroll: self.read_bool("editor.smoothScrolling"),
             scrollbar: self.scrollbar_content(),
             search: self.search_content(),
             search_wrap: None,
